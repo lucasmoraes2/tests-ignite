c584bd8ece3fb1f19ab7d757adebf5d5254b2209
Lucas Moraes <lucasmoraes2@outlook.com.br>
2021-09-05 22:38:22 -0300

    Finalizing transfer feature

--- src/modules/statements/mappers/BalanceMap.ts
+++ src/modules/statements/mappers/BalanceMap.ts
@@ -2,24 +2,6 @@ import { Statement } from "../entities/Statement";
 
 export class BalanceMap {
   static toDTO({statements, balance}: { statements: Statement[], balance: number}) {
-    // const parsedStatement = statement.map(({
-    //   id,
-    //   amount,
-    //   description,
-    //   type,
-    //   created_at,
-    //   updated_at
-    // }) => (
-      // {
-      //   id,
-      //   amount: Number(amount),
-      //   description,
-      //   type,
-      //   created_at,
-      //   updated_at
-      // }
-    // ));
-
     const parsedStatement = statements.map(statement => {
       if (statement.type === "transfer") {
         return {
